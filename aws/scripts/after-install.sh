08ec6249f8fdf90b8993fe558243a44f9331539b
Nalini Jayaram <nalini.jayaram@kainos.com>
2023-02-06 11:22:56 +0000

    adding files to repo

---
+++ aws/scripts/after-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+
+# Copy war file from S3 bucket to tomcat webapp folder
+aws s3 cp s3://poc-githubactions-webappdeploymentbucket-14y7x984wh24d/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+
+
+# Ensure the ownership permissions are correct.
+chown -R tomcat:tomcat /usr/local/tomcat9/webapps
