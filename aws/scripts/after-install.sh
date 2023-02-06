0f02369d8169145e61525fe372225b035dc6e677
Nalini Jayaram <nalini.jayaram@kainos.com>
2023-02-06 11:44:07 +0000

    Adding bucket names

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://poc-githubactions-webappdeploymentbucket-14y7x984wh24d/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://poc-githubactions-webappdeploymentbucket-1dywatds711gl/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
 
 
 # Ensure the ownership permissions are correct.
